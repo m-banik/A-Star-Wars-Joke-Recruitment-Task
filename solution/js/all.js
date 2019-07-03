9d504465acf9e5042927ec8a31dc4aa0e5034022
Marcin Banik <mrn.banik@gmail.com>
2019-07-03 16:02:18 +0200

    Update all.js- change click event to submit event

--- solution/js/all.js
+++ solution/js/all.js
@@ -1,5 +1,5 @@
 const audio = new Audio("../zadanie/imperial_march.mp3");
-const btnHandler = e => {
+const submitHandler = e => {
   const inputs = document.querySelectorAll("input:checked");
   if (inputs[0].id !== "checkbox_yoda") {
     e.preventDefault();
@@ -9,10 +9,9 @@ const btnHandler = e => {
 };
 
 const mainFunction = () => {
-  const btn = document.querySelector("button");
   const labels = document.querySelectorAll("label");
   const lis = document.querySelectorAll("li");
-  btn.addEventListener("click", btnHandler);
+  window.addEventListener("submit", submitHandler);
   labels.forEach((label, index) => {
     const paragraph = document.createElement("p");
     paragraph.innerText = label.getAttribute("data-sentence");
